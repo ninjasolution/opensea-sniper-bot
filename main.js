4fdc3ba9b18dcb52909e241e1b732a7234519775
ExpertBC <top1successfulman@gmail.com>
2022-06-03 00:03:36 +1000

    complete

--- main.js
+++ main.js
@@ -9,13 +9,14 @@ const OrderSide = require("opensea-js/lib/types").OrderSide;
 
 var extragas = 0
 const accountAddress = "0x02fc14d01F4E073829276cc2f4f94Fb4EDe1e0c4"
-// const opensea_link = "https://opensea.io/assets/0xec9c519d49856fd2f8133a0741b4dbe002ce211b/2197"
-const opensea_link = "https://testnets.opensea.io/assets/rinkeby/0x3286c95b43e4beffb15d60c51b6b1c7f026e40bd/449"
+const opensea_link = "https://opensea.io/assets/ethereum/0x75335297cb5029c2a9acb2b47507f18ffd48e96c/2417"
+// const opensea_link = "https://testnets.opensea.io/assets/rinkeby/0x3286c95b43e4beffb15d60c51b6b1c7f026e40bd/430"
 const MNEMONIC = "december hedgehog income today portion acquire betray spring cry wheel practice coyote rebel gas climb"
 const privateKeys = ["a58aa0760cc92d52959fefca1982712e6408d98b7a7fad7796d16f90915145ff"];
-const NODE_API_KEY = "5c34343809f04bcb96e9a4aeaf19aeca"
-const NETWORK = "live"
-const API_KEY = "b410249ba9b14309afd104ee97497485" //process.env.API_KEY || ""; // API key is optional but useful if you're doing a high volume of requests.
+const NODE_API_KEY = "a3a311a7d66644ffa18110e797cf65d4"
+const NETWORK = "mainnet"
+// const NETWORK = "live"
+const API_KEY = "f69c0112d1c348d799aee906d7435263" //process.env.API_KEY || ""; // API key is optional but useful if you're doing a high volume of requests.
 const startTimeUTC = "9:21:30";
 
 if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !opensea_link || !accountAddress) {
@@ -39,21 +40,16 @@ if (!startTimeUTC) {
 
 }
 
-const network = NETWORK === "mainnet" || NETWORK === "live" ? "mainnet" : "rinkeby";
+const network = NETWORK === "mainnet"/*  || NETWORK === "live" */ ? "mainnet" : "rinkeby";
+console.log(network)
 
-const providerEngine = new HDWalletProvider({
-  // mnemonic: {
-  //   phrase: MNEMONIC
-  // },
-  privateKeys: privateKeys,
-  providerOrUrl: "https://" + network + ".infura.io/v3/" + NODE_API_KEY
-});
+const providerEngine = new HDWalletProvider(privateKeys, "https://" + network + ".infura.io/v3/" + NODE_API_KEY);
 
 const seaport = new OpenSeaPort(
   providerEngine,
   {
     networkName:
-      NETWORK === "mainnet" || NETWORK === "live"
+      NETWORK === "mainnet" /* || NETWORK === "live" */
         ? Network.Main
         : Network.Rinkeby,
     apiKey: API_KEY,
@@ -62,42 +58,34 @@ const seaport = new OpenSeaPort(
 // Environment setup done
 
 seaport.gasPriceAddition = new BigNumber(extragas);  // add extra gas to current gas price
-const [asset_contract_address, token_id] = helpers.parse_url(opensea_link) // extract asset contract address and token id from the opensea likn
+const [ asset_contract_address, token_id ] = helpers.parse_url(opensea_link) // extract asset contract address and token id from the opensea likn
 
 console.log(asset_contract_address, token_id)
-console.log(seaport.web3ReadOnly);
 
 async function main() {
   console.log("Launched");
 
-  const asset = await seaport.api.getAsset({
-    asset_contract_address, // string
-    token_id, // string | number | null
-  })
+  
+  let order;
+  try {
+    order = await seaport.api.getOrder({   // Extracting order to fulfill
+        asset_contract_address,
+        token_id,
+        side: OrderSide.Sell,
+    });
+  }catch (error) {
+    console.log(error, "------------------------------")
+  }
 
-  console.log(asset)
-
-  // let order;
-  // try {
-  //   order = await seaport.api.getOrder({   // Extracting order to fulfill
-  //       asset_contract_address,
-  //       token_id,
-  //       side: OrderSide.Sell,
-  //   });
-  //   console.log(order)
-  // }catch (error) {
-  //   console.log(error)
-  // }
-
-  // try {
-  //   const transactionHash = await seaport.fulfillOrder({ //Fulfilling order
-  //         order,
-  //         accountAddress,
-  //   });
-  //   console.log(transactionHash);
-  // }catch (error) {
-  //   console.log(error)
-  // }
+  try {
+    const transactionHash = await seaport.fulfillOrder({ //Fulfilling order
+          order,
+          accountAddress,
+    });
+    console.log(transactionHash);
+  }catch (error) {
+    console.log(error)
+  }
 	
   return;
 
