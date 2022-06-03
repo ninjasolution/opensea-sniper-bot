fe7f983d7824a729b01a5056c84943d91676116d
ExpertBC <top1successfulman@gmail.com>
2022-06-03 14:47:53 +1000

    complete

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ const OrderSide = require("opensea-js/lib/types").OrderSide;
 const OpenSeaPort = opensea.OpenSeaPort;
 const Network = opensea.Network;
 
+require('dotenv').config(); 
 
 var extragas = 0
-const accountAddress = "0x02fc14d01F4E073829276cc2f4f94Fb4EDe1e0c4"
-const opensea_link = "https://opensea.io/assets/ethereum/0x75335297cb5029c2a9acb2b47507f18ffd48e96c/2417"
-// const opensea_link = "https://testnets.opensea.io/assets/rinkeby/0x3286c95b43e4beffb15d60c51b6b1c7f026e40bd/430"
-const MNEMONIC = "december hedgehog income today portion acquire betray spring cry wheel practice coyote rebel gas climb"
-const privateKeys = ["a58aa0760cc92d52959fefca1982712e6408d98b7a7fad7796d16f90915145ff"];
-const NODE_API_KEY = "a3a311a7d66644ffa18110e797cf65d4"
-const NETWORK = "mainnet"
-// const NETWORK = "live"
-const API_KEY = "f69c0112d1c348d799aee906d7435263" //process.env.API_KEY || ""; // API key is optional but useful if you're doing a high volume of requests.
+const accountAddress = process.env.ACCOUNT_ADDRESS
+const opensea_link = process.env.OPENSEA_LINK
+const MNEMONIC = process.env.MNEMONIC
+const privateKeys = process.env.PRIVATE_KEYS
+const NODE_API_KEY = process.env.NODE_API_KEY
+const NETWORK = process.env.NETWORK
+const API_KEY = process.env.API_KEY
+
 
 if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !opensea_link || !accountAddress) {
 
@@ -28,8 +28,7 @@ if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !opensea_link || !accountAddress) 
 }
 
 
-
-const network = NETWORK === "mainnet"/*  || NETWORK === "live" */ ? "mainnet" : "rinkeby";
+const network = NETWORK === "mainnet" /* || NETWORK === "live" */ ? "mainnet" : "rinkeby";
 console.log(network)
 
 const providerEngine = new HDWalletProvider(privateKeys, "https://" + network + ".infura.io/v3/" + NODE_API_KEY);
