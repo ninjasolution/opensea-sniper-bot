5b713abdeab96d5cfdfc76d0192a0a07b69bb034
ExpertBC <top1successfulman@gmail.com>
2022-06-07 03:21:30 +1000

    complete

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 
 var extragas = 0
 const accountAddress = process.env.ACCOUNT_ADDRESS
-const asset_contract_addresses = process.env.COLLECTION_ADDRS
-const token_ids = process.env.TOKEN_IDS;
+const asset_contract_addresses = "0x3286c95b43e4beffb15d60c51b6b1c7f026e40bd";
+const token_ids = ["459", "462"];
 const privateKeys = process.env.PRIVATE_KEYS
 const NODE_API_KEY = process.env.NODE_API_KEY
 const NETWORK = process.env.NETWORK
@@ -29,7 +29,6 @@ if (!NODE_API_KEY || !NETWORK || !asset_contract_addresses || !accountAddress) {
 
 const network = NETWORK === "mainnet" /* || NETWORK === "live" */ ? "mainnet" : "rinkeby";
 console.log(network)
-
 const providerEngine = new HDWalletProvider(privateKeys, "https://" + network + ".infura.io/v3/" + NODE_API_KEY);
 
 const seaport = new OpenSeaPort(
@@ -52,63 +51,33 @@ seaport.gasPriceAddition = new BigNumber(extragas);  // add extra gas to current
 
 // Environment setup done
 
-async function firstTransaction() {
-  let order;
-  try {
-    order = await seaport.api.getOrders({   // Extracting order to fulfill
-      asset_contract_address: asset_contract_addresses[0],
-      token_ids: token_ids[0],
-      side: OrderSide.Sell,
-    });
-
-  }catch (error) {
-    console.log("ERROR(API-GET-ORDERS):", error)
-  }
-
-  try {
-    const transactionHash = await seaport.fulfillOrder({ //Fulfilling order
-          order,
-          accountAddress,
-    });
-
-    console.log(transactionHash);
-  }catch (error) {
-    console.log("ERROR(TRANSACTION-FULL-FILL-ORDER)", error)
-  }
-  
-}
-
-async function secondTransaction() {
-  let order;
-  try {
-    order = await seaport.api.getOrders({   // Extracting order to fulfill
-      asset_contract_address: asset_contract_addresses[1],
-      token_ids: token_ids[1],
-      side: OrderSide.Sell,
-    });
-
-  }catch (error) {
-    console.log("ERROR(API-GET-ORDERS):", error)
-  }
-
-  try {
-    const transactionHash = await seaport.fulfillOrder({ //Fulfilling order
-          order,
-          accountAddress,
-    });
-
-    console.log(transactionHash);
-  }catch (error) {
-    console.log("ERROR(TRANSACTION-FULL-FILL-ORDER)", error)
-  }
-  
-}
 
 async function main() {
   console.log("Launched");
 
-  await firstTransaction();
-  await secondTransaction();
+  let order;
+  try {
+    order = await seaport.api.getOrders({   // Extracting order to fulfill
+      asset_contract_address: asset_contract_addresses,
+      token_ids: token_ids,
+      side: OrderSide.Sell
+    });
+
+  }catch (error) {
+    console.log("ERROR(API-GET-ORDERS):", error)
+  }
+
+  try {
+    const transactionHash = await seaport.fulfillOrder({ //Fulfilling order
+          order,
+          accountAddress,
+    });
+
+    console.log(transactionHash);
+  }catch (error) {
+    console.log("ERROR(TRANSACTION-FULL-FILL-ORDER)", error)
+  }
+  
   
   return;
 };
